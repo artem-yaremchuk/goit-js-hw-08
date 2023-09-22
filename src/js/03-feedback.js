59f5087505b0374aaaebb2b0366dbf9c8add2db0
artem-yaremchuk <119439325+artem-yaremchuk@users.noreply.github.com>
2023-09-22 18:16:13 +0300

    update#4

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector(".feedback-form");
+const emailInput = document.querySelector("input");
+const messageTextarea = document.querySelector("textarea");
+
+const storedFormState =
+  JSON.parse(localStorage.getItem("feedback-form-state")) ?? {};
+emailInput.value = storedFormState.email ?? "";
+messageTextarea.value = storedFormState.message ?? "";
+
+console.log(storedFormState);
+
+const formState = {
+  email: emailInput.value ?? "",
+  message: messageTextarea.value ?? "",
+};
+
+const updateFormState = throttle(() => {
+  localStorage.setItem("feedback-form-state", JSON.stringify(formState));
+}, 500);
+
+form.addEventListener("input", (event) => {
+  const { name, value } = event.target;
+  formState[name] = value;
+  updateFormState();
+});
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  localStorage.removeItem("feedback-form-state");
+  emailInput.value = "";
+  messageTextarea.value = "";
+
+  console.log(formState);
+});
